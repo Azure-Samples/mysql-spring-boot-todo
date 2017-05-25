1a706cab5ec233733030d87e96e38115d6ed6a2f
Robert Outlaw <routlaw@microsoft.com>
2017-05-25 15:35:05 -0700

    change over to executable war

--- src/main/java/com/example/fabrikam/TodoDemo/TodoDemoApplication.java
+++ src/main/java/com/example/fabrikam/TodoDemo/TodoDemoApplication.java
@@ -6,12 +6,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 
-public class TodoDemoApplication {
+public class TodoDemoApplication extends SpringBootServletInitializer {
 
 	private static final Logger log = LoggerFactory.getLogger(TodoDemoApplication.class);
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TodoDemoApplication.class);
+    }
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoDemoApplication.class, args);
